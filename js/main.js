c3684c7c5a06764985b7bd80eab201f85de9eb23
husanovjamshid <husanovjamshid11@gmail.com>
2022-11-27 06:54:48 +0300

    finished project

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var title = document.querySelector('.title')
+var userName = prompt("Assalomu alaykum ismingizni kiriting: ")
+var userMoney = Number(prompt("Sayohat uchun pulingizni kiriting: "))
+
+var ticket = 500
+var hotel = 250
+var mousem = 120
+
+var totalUsd = (500 + 250) * 11225
+var totalEuro = (120) * 11686
+
+var allTotal = totalUsd + totalEuro
+
+
+
+if (userMoney >= allTotal) {
+    title.textContent = `Oq yo'l hurmatli ${userName} Sayohatingiz maroqli o'tsin!!!`
+}else {
+    title.textContent = `Hurmatli ${userName} Sayohatingiz uchun mablag' yetarli emas ):`
+}
